883a4ad8efcfa7a65ed0f7685cd26a967c66fb86
Colbie <oddlyk1@gmail.com>
2018-11-18 13:40:06 -0500

    Init push

---
+++ ToOT/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToOT
+{
+    public partial class Main_frm : Form
+    {
+        public Main_frm()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_frm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Vol1_pic_Click(object sender, EventArgs e)
+        {
+            //Vol. 1 Tome
+        }
+
+        private void Vol2_pic_Click(object sender, EventArgs e)
+        {
+            //Vol. 2 Tome
+        }
+
+        private void Vol3_pic_Click(object sender, EventArgs e)
+        {
+            //Vol. 3 Tome
+        }
+
+        private void Vol4_pic_Click(object sender, EventArgs e)
+        {
+            //Vol. 4 Tome
+        }
+    }
+}
